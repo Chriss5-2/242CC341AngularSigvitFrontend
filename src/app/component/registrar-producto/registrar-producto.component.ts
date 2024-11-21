2ba22367100edeefc489f46a4ed0a6fee2696e08
Christian Luna <christiangiovannixd@gmail.com>
2024-11-21 13:25:33 -0500

    complete add form registrar-producto cab

--- src/app/component/registrar-producto/registrar-producto.component.ts
+++ src/app/component/registrar-producto/registrar-producto.component.ts
@@ -3,11 +3,12 @@ import { ProductoService } from '../../service/producto.service';
 import { IProductoResponse } from '../../model/producto-response';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar-producto',
   standalone: true,
-  imports: [CommonModule, NgxPaginationModule],
+  imports: [CommonModule, NgxPaginationModule, ReactiveFormsModule],
   templateUrl: './registrar-producto.component.html',
   styleUrl: './registrar-producto.component.css'
 })
@@ -15,7 +16,18 @@ export class RegistrarProductoComponent {
   title = 'Registrar Producto';
   productoArray: IProductoResponse[]=[];
   page:number=1;
-  constructor(private productoService:ProductoService){}
+  productoForm:FormGroup;
+
+  constructor(private productoService:ProductoService){
+    this.productoForm = new FormGroup({
+      idProducto: new FormControl(''),
+      nombre: new FormControl('', [Validators.required,Validators.minLength(2)]),
+      precioVenta: new FormControl(''),
+      stock: new FormControl(''),
+      categoriaNombre: new FormControl('', [Validators.required,Validators.minLength(2)]),
+      categoriaDescripcion: new FormControl('', [Validators.required,Validators.minLength(2)]),
+    });
+  }
   ngOnInit():void{
     this.getProductos();
   }
