30476cae7ec32804bb8af7d5a8a8afe1b39ac352
Christian Luna <christiangiovannixd@gmail.com>
2024-12-02 01:14:31 -0500

    Nuevo inicio agregado/botón de registrar producto agregado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegistrarProductoComponent } from './component/registrar-producto/registrar-producto.component';
+import { NavbarComponent } from "./component/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegistrarProductoComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
